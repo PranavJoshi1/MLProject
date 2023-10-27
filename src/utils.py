a45db613bbf0b8ebdb2d2b2672317f31fe11cd3f
Pranav Joshi <joshipranavc@gmail.com>
2023-10-27 17:22:03 +0530

    Added hyperparameter tunning but commented it due to some error is coming

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ import pickle
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -27,9 +28,18 @@ def evaluate_models(X_train, y_train, X_test,y_test, models):
         report = {}
         
         for i in range(len(list(models))):
+            
             model = list(models.values())[i]
             
+            #para = param[list(models.values())[i]]
+                         
+            #gs = GridSearchCV(model, para,cv = 3)
+            #gs.fit(X_train, y_train)
+            
             model.fit(X_train, y_train)
+            
+            #model.set_params(**gs.best_params_)
+            #model.fit(X_train, y_train)
             
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
