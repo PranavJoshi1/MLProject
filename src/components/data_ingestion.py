a2be9dab89b0927ad6e9bd099247a4b405908020
Pranav Joshi <joshipranavc@gmail.com>
2023-10-27 16:46:33 +0530

    Adding model trainer and evaluation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from logger import logging
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 # from src.exception import CustomException
 # from src.logger import logging
@@ -67,5 +69,8 @@ if __name__ == "__main__":
     
     data_trasformation = DataTransformation()
     
-    data_trasformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_trasformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
